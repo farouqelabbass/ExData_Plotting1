89deb4780fcaf95e485bbec14f79a75840782ba9
Farouq <farouge1@gmail.com>
2020-10-15 14:37:49 +0100

    Week 1 Course Project

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", height = 480, width = 480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
